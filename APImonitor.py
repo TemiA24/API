6679ef366efe0c419f5e2f55549b16ebd757dd4f
Temi Adepetun <131882718+TemiA24@users.noreply.github.com>
2023-08-25 11:59:05 +0100

    Add files via upload

--- APImonitor.py
+++ APImonitor.py
@@ -1,52 +1,88 @@
 import requests
 import time
+import os
+from dotenv import load_dotenv
 
-FMP_API_KEY = "becda64896abed95e027f6df4b8d13c1"
-
-IFTT_WEBHOOK_KEY = "jYf4ycDEDmCJ8a0aGPQz1no7EXG5f_QYCiwHETGUOVM"
-
+load_dotenv()
 stock_symbols = ["AAPL", "TSLA", "MSFT", "GOOGL", "NKE"]
 
 price_change_threshold = 0.25
-interval_seconds = 60
+interval_seconds = 300
 
-IFTT_WEBHOOK_EVENT = "stock_price_drop"
+API_KEY = os.getenv("API_KEY")
+IFTT_WEBHOOK_KEY = os.getenv("IFTT_WEBHOOK_KEY")
 
-def get_stock_price(symbol):
-    url = "https://financialmodelingprep.com/api/v3/quote/{syymbol}?apikey={becda64896abed95e027f6df4b8d13c1}"
+
+historical_prices = "historical price"
+seven_day_average = "calcualte seven day average"
+IFTT_WEBHOOK_EVENT = "name of event"
+avg_price = "average price after 7 days"
+calculate_average_price = "average price sum"
+
+
+
+
+
+def get_stock_price(symbol, API_KEY):
+    
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&{symbol}=IBM&apikey={API_KEY}"
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo"
     response = requests.get(url)
     data = response.json()
-    return float(data[0]['price'])
+    latest_price = list(data["Time Series(5min)"].values())[0]["4. close"]
+    print (float(latest_price))
+    return float(latest_price)
 
-def send_notification(stock_symbol, current_price, price_change):
-    IFTT_WEBHOOK_EVENT = {
+
+for symbol in stock_symbols:
+    price = get_stock_price(symbol, API_KEY)
+    
+        
+    
+get_stock_price("AAPL", API_KEY)   
+
+
+
+for symbol in stock_symbols:
+    
+    
+        
+ def send_notification(stock_symbol, current_price, price_change, event):
+    IFTT_WEBHOOK_DATA = {
         "value1":stock_symbol,
         "value2":current_price,
         "value3":price_change
     }
+    
+    url = f"https://maker.ifttt.com/trigger/{IFTT_WEBHOOK_EVENT}/json/with/key/{IFTT_WEBHOOK_KEY}"
+    print(event)
+    response = requests.post(url, json=IFTT_WEBHOOK_DATA)
+    if response.status_code == 200:
+        print("notification sent successfully")
+    else:
+        print("notification sending failed")
 
-url = "https://maker.ifttt.com/trigger/stock_price_drop/with/key/jYf4ycDEDmCJ8a0aGPQz1no7EXG5f_QYCiwHETGUOVM"
-response = requests.post(url, json=IFTT_WEBHOOK_EVENT)
-if response.status_code == 200:
-    print("notification sent successfully")
-else:
-    print("notification sending failed")
 
 while True:
-   for symbol in stock_symbols:
-       current_price = get_stock_price
-       prev_price = current_price
-       price_change = current_price - prev_price
+    for symbol in stock_symbols:
+        current_price = get_stock_price(symbol) 
+        historical_prices[symbol].append(current_price)
+        
 
-   if price_change_threshold <= -price_change_threshold:
-    send_notification(symbol, current_price, price_change)
+    if price_change_threshold <= -price_change_threshold:
+       send_notification(symbol, current_price,  "stock_price_drop")
     
-    seven_day_average = get_stock_price(symbol)
-   if current_price < seven_day_average:
-            send_notification(symbol, current_price, seven_day_average)
+    
+    if current_price < seven_day_average:
+       send_notification(symbol, current_price)
+       
+    if len(historical_prices[symbol]) > 7:
+            historical_prices[symbol].pop(0)
+        
+        
+    function = calculate_average_price(symbol)
+    if avg_price is not None and current_price < avg_price:
+        send_notification(symbol, current_price, f"below 7-day average ({avg_price:.2f})")
             
-   
-
-
-
+    time.sleep(300)
 
